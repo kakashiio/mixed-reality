eddde82b86f73595b440517aeac82106323118ea
kakashi <junhong.cai@kunlun-inc.com>
2024-07-12 23:55:26 +0800

    Passthrough basic shader

---
+++ Assets/Scripts/PassthroughPlaneScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PassthroughPlaneScript : MonoBehaviour
+{
+    void Start()
+    {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_ANDROID
+        OVRManager.eyeFovPremultipliedAlphaModeEnabled = false;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
